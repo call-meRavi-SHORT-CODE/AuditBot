45abc1104ae46dd43ae01310281f2b71e4a5cca4
call-meRavi-SHORT-CODE <ravikrishnaj25@gmail.com>
2024-05-23 09:06:38 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import random
+import time
+
+from chat import get_response
+
+st.title("AuditBot 💬 ")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Generate assistant response
+    assistant_response = get_response(prompt)
+
+    # Add a slight delay before displaying the assistant response
+    time.sleep(0.005)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
+
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": assistant_response})
